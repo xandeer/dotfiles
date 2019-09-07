858e3641d8e6ed76bdc3b2d9eabfedda93a9fa0a
Kevin <kkxandeer@gmail.com>
2019-09-07 10:57:47 +0800

    chore: rm .yas-compiled-snippets.el

--- doom/snippets/emacs-lisp-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `emacs-lisp-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'emacs-lisp-mode
-                     '(("def" "(defun ${1:fun} (${2:args})\n  \"${3:docstring}\"\n  ${4:(interactive${5: \"${6:P}\"})}\n  $0)" "defun" nil nil nil "/Users/kevin/.config/doom/snippets/emacs-lisp-mode/defun" nil nil)))
-
-
-;;; Do not edit! File generated at Sat Jul  6 16:51:36 2019
--- doom/snippets/js2-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `js2-mode'
-;;; Do not edit! File generated at Sat Jul  6 16:51:36 2019
