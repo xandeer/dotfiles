8548d933617b82ebd7c18627af32fd6dc37c8321
kevin <kkxandeer@gmail.com>
2019-09-30 10:40:35 +0800

    fix: telega-proxies settings

--- doom/modules/xandeer/telega/config.el
+++ doom/modules/xandeer/telega/config.el
@@ -8,12 +8,16 @@
   (telega-mode-line-mode 1)
   (set-popup-rule! "^\\*Telega Root" :side 'left :size 0.2 :quit nil :select t)
   (set-popup-rule! "^◀\\[.*@.*\\]" :side 'right :size 0.6 :quit nil :select t)
-  (setq ;telega-use-tracking t
-        ;; telega-known-inline-bots '("@")
-   telega-proxies (list '(:sever "127.0.0.1" :port 8010 :enable t :type (:@type "proxyTypeHttp")))
-        telega-sticker-set-download t
-        telega-chat-button-width 28
-        telega-root-fill-column 48)
+  (setq
+   telega-proxies
+    (list
+     '(:server "127.0.0.1" :port 8010 :enable t
+               :type (:@type "proxyTypeHttp" :http_only t)))
+   telega-sticker-set-download t
+   telega-chat-button-width 28
+   ; telega-use-tracking t
+   ; telega-known-inline-bots '("@")
+   telega-root-fill-column 48)
   (when (featurep! :completion ivy)
     (load! "+ivy"))
   (when (featurep! :completion company)
