61d74da95759acabe8bd3332ed67751283e4c9de
Kevin Du <kkxandeer@gmail.com>
2019-11-02 11:03:50 +0800

    feat: add surfingkeys settings

---
+++ surfingkeys/xandeer.js
@@ -0,0 +1,40 @@
+map('J', 'D');
+map('K', 'S');
+map('t', 'T');
+map('F', 'af');
+map('h', 'E');
+map('l', 'R');
+
+
+// theme
+settings.theme = `
+.sk_theme {
+	background: #100a14dd;
+	color: #4f97d7;
+}
+.sk_theme tbody {
+	color: #292d;
+}
+.sk_theme input {
+	color: #d9dce0;
+}
+.sk_theme .url {
+	color: #2d9574;
+}
+.sk_theme .annotation {
+	color: #a31db1;
+}
+.sk_theme .omnibar_highlight {
+	color: #333;
+	background: #ffff00aa;
+}
+.sk_theme #sk_omnibarSearchResult ul>li:nth-child(odd) {
+	background: #5d4d7a55;
+}
+.sk_theme #sk_omnibarSearchResult ul>li.focused {
+	background: #5d4d7aaa;
+}
+.sk_theme #sk_omnibarSearchResult .omnibar_folder {
+	color: #a31db1;
+}
+`;
