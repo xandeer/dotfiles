3d976711cfc949717c122ca98002571b27ffb4db
Kevin Du <kkxandeer@gmail.com>
2020-04-17 14:47:35 +0800

    feat: skip set proxy on termux

--- zsh/.config/zsh/proxy.zsh
+++ zsh/.config/zsh/proxy.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
-set is_termux = command -v termux-setup-storage
-if test -z "$is_termux"; then
-  export px=http://127.0.0.1:8010
-  export https_proxy=$px
-  export http_proxy=$px
-  export all_proxy=$px
-fi
+function set_proxy() {
+  local is_termux=$(command -v termux-setup-storage)
+  if test -z "$is_termux"; then
+    export px=http://127.0.0.1:8010
+    export https_proxy=$px
+    export http_proxy=$px
+    export all_proxy=$px
+  fi
+}
+
+set_proxy
