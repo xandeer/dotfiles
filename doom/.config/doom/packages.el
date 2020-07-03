1151b72443702519a1adf3aac5e60d76297916e9
Kevin Du <kkxandeer@gmail.com>
2020-07-03 12:47:15 +0800

    refactor: use doom literate to tangle

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -1,45 +1,45 @@
-(package! disable-mouse)
+  (package! disable-mouse)
 
-(package! keyfreq)
+  (package! keyfreq)
 
-(package! youdao-dictionary)
+  (package! youdao-dictionary)
 
-(package! deft)
+  (package! deft)
 
-(package! pyim
-  :recipe
-  (:host github :repo "tumashu/pyim"
-       :files (:defaults "*.el")))
-(package! posframe)
+  (package! pyim
+    :recipe
+    (:host github :repo "tumashu/pyim"
+  	 :files (:defaults "*.el")))
+  (package! posframe)
+  
+  ;; (package! pyim-basedict
+  ;;   :recipe
+  ;;   (:host github :repo "tumashu/pyim-basedict"
+  ;;          :files (:defaults "pyim-basedict.el" "pyim-basedict.pyim")))
+  
+  (package! pyim-greatdict
+    :recipe
+    (:host github :repo "tumashu/pyim-greatdict"
+  	 :files (:defaults "pyim-greatdict.el" "pyim-greatdict.pyim.gz")))
 
-;; (package! pyim-basedict
-;;   :recipe
-;;   (:host github :repo "tumashu/pyim-basedict"
-;;          :files (:defaults "pyim-basedict.el" "pyim-basedict.pyim")))
+  (package! cal-china-x
+    :recipe
+    (:host github :repo "xwl/cal-china-x"
+  	 :files (:defaults "*.el")))
 
-(package! pyim-greatdict
-  :recipe
-  (:host github :repo "tumashu/pyim-greatdict"
-       :files (:defaults "pyim-greatdict.el" "pyim-greatdict.pyim.gz")))
+  (package! yasnippet)
 
-(package! cal-china-x
-  :recipe
-  (:host github :repo "xwl/cal-china-x"
-       :files (:defaults "*.el")))
+  (package! nord-theme)
 
-(package! yasnippet)
+  (package! ivy-posframe
+    :disable t
+    :recipe
+    (:host github :repo "tumashu/ivy-posframe"
+  	 :files (:defaults "*.el")))
 
-(package! nord-theme)
-
-(package! ivy-posframe
-  :disable t
-  :recipe
-  (:host github :repo "tumashu/ivy-posframe"
-       :files (:defaults "*.el")))
-
-(package! telega
-  :recipe
-  (:host github
-   :repo "zevlg/telega.el"
-   :branch "telega-tdlib-150"
-   :files (:defaults "README.md" "etc" "server" "Makefile" "test.el")))
+  (package! telega
+    :recipe
+    (:host github
+     :repo "zevlg/telega.el"
+     :branch "telega-tdlib-150"
+     :files (:defaults "README.md" "etc" "server" "Makefile" "test.el")))
