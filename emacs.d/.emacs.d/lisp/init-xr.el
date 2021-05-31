f7d2bf7d4eb982391623712cd08741f3ca8dab9a
Kevin Du <kkxandeer@gmail.com>
2021-05-31 18:29:49 +0800

    feat: add functiions to remove links

--- emacs.d/.emacs.d/lisp/init-xr.el
+++ emacs.d/.emacs.d/lisp/init-xr.el
@@ -2,10 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun xr/replace-in-buffer (old new)
-  "Replace the string OLD with string NEW."
+(defun xr/replace-in-buffer (old new &optional forward)
+  "Replace the string OLD with string NEW.
+When FORWARD, don't go to min point."
+  (unless forward (setq forward nil))
   (save-excursion
-    (goto-char (point-min))
+    (unless forward
+      (goto-char (point-min)))
     (while (re-search-forward old nil t)
       (replace-match new))))
 
@@ -22,6 +25,21 @@
 (defun xr/org-heading-beginning-p ()
   "Whether the point is at beginning of a heading."
   (and (org-at-heading-p) (= (line-beginning-position) (point))))
+
+(defun xr/remove-links-forward ()
+  "Remove links in after current point."
+  (interactive)
+  (xr/remove-links t))
+
+(defun xr/remove-links-in-buffer ()
+  "Remove links in the current buffer."
+  (interactive)
+  (xr/remove-links nil))
+
+(defun xr/remove-links (forward)
+  "Remove links after current point.
+When FORWARD, don't go to min point."
+  (xr/replace-in-buffer "\\[\\[.*\\]\\[\\(.*\\)\\]\\]" "\\1" forward))
 
 (provide 'init-xr)
 ;;; init-xr.el ends here
