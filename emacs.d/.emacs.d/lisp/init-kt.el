9ed383b0006ade3261d865bc7ea989ae0d6c043f
Kevin Du <kkxandeer@gmail.com>
2021-06-07 14:42:17 +0800

    feat: update kotlin config

--- emacs.d/.emacs.d/lisp/init-kt.el
+++ emacs.d/.emacs.d/lisp/init-kt.el
@@ -2,10 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(leaf kotlin
+(leaf kt
   :straight kotlin-mode flycheck-kotlin
   :hook
-  (kotlin-mode-hook . flycheck-kotlin-setup))
+  (kotlin-mode-hook . flycheck-kotlin-setup)
+  :init
+  (add-to-list 'exec-path (expand-file-name
+        "server/build/install/server/bin"
+        "~/projects/others/kotlin-language-server"))
+  :custom
+  (kotlin-tab-width . 2))
 
 (provide 'init-kt)
 ;;; init-kt.el ends here
