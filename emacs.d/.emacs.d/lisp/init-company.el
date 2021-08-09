b9e211d59533bbff0d64889cd03522c74a44b9d7
Kevin Du <kkxandeer@gmail.com>
2021-08-09 15:37:47 +0800

    feat: simplify company

--- emacs.d/.emacs.d/lisp/init-company.el
+++ emacs.d/.emacs.d/lisp/init-company.el
@@ -5,38 +5,19 @@
 (leaf company
   :straight t
   :hook (after-init-hook . global-company-mode)
-  :init
-  (add-to-list 'completion-styles 'initials t)
   :bind
   (:company-active-map
-   ("C-o"        . company-search-kill-others)
-   ("C-n"        . company-select-next)
-   ("C-p"        . company-select-previous)
-   ("C-h"        . company-quickhelp-manual-begin)
-   ("C-S-h"      . company-show-doc-buffer)
-   ("M-s"        . company-filter-candidates)
-   ("C-s"        . counsel-company)
-   ([C-tab]      . xr/company-complete)
-   ([tab]        . company-complete-common-or-cycle)
-   ([backtab]    . company-select-previous))
-  (:company-search-map
-   ("C-n"        . company-search-repeat-forward)
-   ("C-p"        . company-search-repeat-backward))
+   ("M-s" . company-filter-candidates)
+   ("C-s" . counsel-company))
   :custom
+  (company-idle-delay                 . .2)
+  (company-show-numbers               . t)
   (company-dabbrev-downcase           . nil)
   (company-dabbrev-ignore-case        . 'keep)
-  (company-dabbrev-code-ignore-case   . t)
-  (company-dabbrev-code-other-buffers . 'all)
-  (company-idle-delay                 . .2)
-  (company-tooltip-align-annotations  . t)
   (company-minimum-prefix-length      . 1)
-  :config
-  (defun xr/company-short ()
-    (company-search-abort)
-    (company-filter-candidates))
-  (dolist (backend '(company-eclim company-semantic))
-    (delq backend company-backends)))
-;; (diminish 'company-mode))
+  (company-dabbrev-code-ignore-case   . t)
+  (company-tooltip-align-annotations  . t)
+  (company-dabbrev-code-other-buffers . 'all))
 
 (provide 'init-company)
 ;;; init-company.el ends here
