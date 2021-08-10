4b070eb0589adbb5a3265b516bea86631e462524
Kevin Du <kkxandeer@gmail.com>
2021-08-11 07:48:04 +0800

    fix: disable visual line mode doesn't work

--- emacs.d/.emacs.d/lisp/init-org-journal.el
+++ emacs.d/.emacs.d/lisp/init-org-journal.el
@@ -8,16 +8,17 @@
   :hook
   (org-journal-mode-hook . valign-mode)
   ;; visual line mode will override the org-special-ctrl-a/e
-  (org-journal-mode-hook . (lambda () (visual-line-mode -1)))
-  :bind
-  ("C-c x t" . org-journal-open-current-journal-file)
+  (org-journal-mode-hook . disable-visual-line-mode)
   :custom
   (org-journal-follow-mode . t)
   (org-journal-file-format . "%Y-%m-%d.org")
   (org-journal-time-format . "<%Y-%m-%d %R> ")
   (org-journal-date-format . "%B %d")
   (org-journal-dir         . `,(expand-file-name "journal" org-directory))
-  (org-journal-file-header . ":PROPERTIES:\n:CATEGORY: Journal\n:END:\n#+TITLE: %B %d\n#+STARTUP: content\n\n"))
+  (org-journal-file-header . ":PROPERTIES:\n:CATEGORY: Journal\n:END:\n#+TITLE: %B %d\n#+STARTUP: content\n\n")
+  :config
+  (defun disable-visual-line-mode ()
+    (visual-line-mode -1)))
 
 (provide 'init-org-journal)
 ;;; init-org-journal.el ends here
