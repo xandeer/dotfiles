8e32160c0b3dce54ca18b1aff9c1e5cb3bec7aee
Kevin Du <kkxandeer@gmail.com>
2021-08-11 10:23:46 +0800

    feat: update org special ctrl a/e

--- emacs.d/.emacs.d/lisp/init-org.el
+++ emacs.d/.emacs.d/lisp/init-org.el
@@ -87,7 +87,7 @@
   (org-global-properties
    . '(("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
        ("STYLE_ALL"  . "habit")))
-  (org-special-ctrl-a/e   . 'reversed)
+  (org-special-ctrl-a/e   . t)
   (org-special-ctrl-k     . t)
   (org-use-speed-commands . t)
   (org-speed-commands-user
