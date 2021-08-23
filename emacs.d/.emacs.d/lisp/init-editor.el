3583cb150c301191ddd6ae30d860ea374a3d5b14
Kevin Du <kkxandeer@gmail.com>
2021-08-23 14:06:54 +0800

    refactor: delete the useless packages

--- emacs.d/.emacs.d/lisp/init-editor.el
+++ emacs.d/.emacs.d/lisp/init-editor.el
@@ -12,17 +12,11 @@
   (deactivate-mark-hook . hungry-delete-mode)
   (after-init-hook . global-hungry-delete-mode))
 
-(leaf ansi-color
-  :disabled t
+(leaf easy-kill
   :straight t
-  :commands colourise-compilation-buffer
-  :hook (compilation-filter-hook . colourise-compilation-buffer)
-  :config
-  (eval-and-compile
-    (defun colourise-compilation-buffer ()
-      (when (eq major-mode 'compilation-mode)
-        (ansi-cOLOR-APPLY-on-region compilation-filter-start
-                                    (point-max))))))
+  :bind
+  ([remap kill-ring-save] . easy-kill)
+  ([remap mark-sexp]      . easy-mark))
 
 (leaf jka-cmpr
   :hook (after-init-hook . auto-compression-mode))
@@ -40,12 +34,6 @@
   (org-capture-mode-hook             . auto-save-disable)
   (org-capture-prepare-finalize-hook . auto-save-enable)
   (after-init-hook                   . auto-save-enable))
-
-(leaf easy-kill
-  :straight t
-  :bind
-  ([remap kill-ring-save] . easy-kill)
-  ([remap mark-sexp]      . easy-mark))
 
 (leaf eldoc-box
   :straight t
@@ -225,63 +213,12 @@
          ("<f8>" . symbol-overlay-remove-all)
          ("<f7>" . symbol-overlay-mode)))
 
-(straight-register-package
- '(point-history :type git
-                 :host github
-                 :repo "blue0513/point-history"))
-
-(leaf point-history
-  :straight t
-  :hook after-init-hook
-  :bind
-  ("C-c C-/" . point-history-show)
-  :custom (point-history-ignore-buffer . "^ \\*Minibuf\\|^ \\*point-history-show*"))
-
-(leaf undo-propose
-  :bind
-  ("C-c u" . undo-propose)
-  :config
-  (undo-propose-wrap undo)
-  (undo-propose-wrap redo))
-
 (leaf uniquify
   :custom
   (uniquify-buffer-name-style   . 'reverse)
   (uniquify-separator           . " • ")
   (uniquify-after-kill-buffer-p . t)
   (uniquify-ignore-buffers-re   . "^\\*"))
-
-(leaf vlf
-  :commands ffap-vlf
-  :init
-  (defun ffap-vlf ()
-    "Find file at point with VLF."
-    (interactive)
-    (let ((file (ffap-file-at-point)))
-      (unless (file-exists-p file)
-        (error "File does not exist: %s" file))
-      (vlf file))))
-
-(leaf whitespace
-  :disabled t
-  :straight t
-  :init
-  (defun no-trailing-whitespace ()
-    "Turn off display of trailing whitespace in this buffer."
-    (setq show-trailing-whitespace nil))
-  :custom
-  (whitespace-line-column . 76)
-  (whitespace-style
-   . '(face spaces tabs space-mark tab-mark empty))
-  :hook
-  ;; But don't show trailing whitespace in SQLi, inf-ruby etc.
-  ((picture-mode-hook
-    special-mode-hook
-    Info-mode-hook
-    eww-mode-hook
-    minibuffer-setup-hook
-    fundamental-mode-hook) . no-trailing-whitespace)
-  (after-init-hook . global-whitespace-mode))
 
 (leaf writeroom-mode
   :preface
