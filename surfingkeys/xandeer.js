5b27e700bccf046caa69b0fa8eda6f43bc037bd0
Kevin Du <kkxandeer@gmail.com>
2021-08-27 11:05:22 +0800

    manual: [2021-08-27 Fri 11:05:22]

--- surfingkeys/xandeer.js
+++ surfingkeys/xandeer.js
@@ -1,13 +1,47 @@
 map('J', 'D');
 map('K', 'S');
 map('t', 'T');
-map('f', 'af');
+map('F', 'af');
 map('h', 'E');
 map('l', 'R');
 map('j', 'd');
 map('k', 'e');
 
-settings.focusFirstCandidate = true
+settings.focusFirstCandidate = true;
+settings.nextLinkRegex = /((>>|next|pre)+)/i;
+settings.prevLinkRegex = /((<<|prev(ious)?)+)/;
+
+addSearchAliasX('c', 'grep.app', 'https://grep.app/search?q=', 's', 'https://grep.app/search?q=', function(response) {
+    var res = JSON.parse(response.text);
+    return res.map(function(r){
+        return r.phrase;
+    });
+});
+mapkey('oc', 'grep.app', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "c"});
+});
+addSearchAliasX('w', 'wikipedia en', 'https://en.wikipedia.org/wiki/', 's', 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&namespace=0&limit=40&search=', function(response) {
+    return JSON.parse(response.text)[1];
+});
+addSearchAliasX('f', 'wikipedia cn', 'https://zh.wikipedia.org/wiki/', 's', 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&namespace=0&limit=40&search=', function(response) {
+    return JSON.parse(response.text)[1];
+});
+mapkey('ow', 'wikipedia en', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "w"});
+});
+mapkey('of', 'wikipedia cn', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "f"});
+});
+map('oo', 'og');
+mapkey('ou', 'github', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "h"});
+});
+mapkey('oa', 'Open google translate to cn', () => {
+    tabOpenLink("https://translate.google.com/?sl=auto&tl=zh-CN&text=")
+});
+mapkey('ob', 'Open google translate to en', () => {
+    tabOpenLink("https://translate.google.com/?sl=auto&tl=en&text=")
+});
 
 // theme
 settings.theme = `
