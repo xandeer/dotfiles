faa3bde993ced2c1bbc820e8e40f26c7c3d20aa6
Kevin Du <kkxandeer@gmail.com>
2021-08-28 14:04:36 +0800

    feat: update setting for prescient

--- emacs.d/.emacs.d/lisp/init-avy.el
+++ emacs.d/.emacs.d/lisp/init-avy.el
@@ -34,48 +34,12 @@
                "")
               "\\)"))))
 
-  (defun prescient-filter-regexps (query &optional with-groups)
-    "Convert QUERY to list of regexps.
-Each regexp must match the candidate in order for a candidate to
-match the QUERY.
+  (cl-defun xr/prescient-pinyin-regexp (query &key with-group
+                                              &allow-other-keys)
+    (prescient-with-group (pinyin-to-utf8 query)
+                          (eq with-group 'all)))
 
-If WITH-GROUPS is non-nil, enclose the initials in initialisms
-with capture groups. If it is the symbol `all', additionally
-enclose literal substrings with capture groups."
-    (mapcar
-     (lambda (subquery)
-       (string-join
-        (cl-remove
-         nil
-         (mapcar
-          (lambda (method)
-            (pcase method
-              (`literal
-               (prescient--with-group
-                (char-fold-to-regexp subquery)
-                (eq with-groups 'all)))
-              (`initialism
-               (prescient--initials-regexp subquery with-groups))
-              (`regexp
-               (ignore-errors
-                 ;; Ignore regexp if it's malformed.
-                 (string-match-p subquery "")
-                 subquery))
-              (`fuzzy
-               (prescient--fuzzy-regexp subquery with-groups))
-              (`prefix
-               (prescient--prefix-regexp subquery with-groups))
-              (`pinyin
-               (pinyin-to-utf8 subquery))))
-          (pcase prescient-filter-method
-            ;; We support `literal+initialism' for backwards
-            ;; compatibility.
-            (`literal+initialism '(literal initialism))
-            ((and (pred listp) x) x)
-            (x (list x))))
-         :test #'eq)
-        "\\|"))
-     (prescient-split-query query))))
+  (add-to-list 'prescient-filter-alist '(pinyin . xr/prescient-pinyin-regexp)))
 
 (leaf avy
   :straight t
