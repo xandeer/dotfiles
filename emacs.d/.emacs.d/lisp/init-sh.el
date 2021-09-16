e29a7e6dc3bf75671469dfc130ca5a22b2b21059
Kevin Du <kkxandeer@gmail.com>
2021-09-16 15:36:33 +0800

    feat: eshell and makefile-executor
    - disalbe company-mode in eshell
    - bind search history in eshell on C-s
    - add eshell z
    - add makefile executor

--- emacs.d/.emacs.d/lisp/init-sh.el
+++ emacs.d/.emacs.d/lisp/init-sh.el
@@ -2,16 +2,41 @@
 ;;; Commentary:
 ;;; Code:
 
-(leaf sh
-  :straight company-shell
+(leaf company-shell
+  :straight t
   :after sh-script
   :config
-  (setq company-shell-delete-duplicates t))
+  (add-to-list 'company-backends 'company-shell))
 
-(leaf eshell
+(leaf eshell-mode
   :after org
+  :hook (eshell-mode-hook . (lambda () (define-key eshell-mode-map [(control ?s)] 'xr/eshell-search-history) (company-mode -1)))
   :custom
-  `(eshell-aliases-file . ,(xr/expand-note "etc/eshell.alias")))
+  `(eshell-aliases-file . ,(xr/expand-note "etc/eshell.alias"))
+  :config
+  (defun xr/eshell-search-history ()
+    "Search the eshell command history with ivy."
+    (interactive)
+    (require 'em-hist)
+    (let* ((ivy-completion-beg (eshell-bol))
+           (ivy-completion-end (point-at-eol))
+           (input (buffer-substring-no-properties
+                   ivy-completion-beg
+                   ivy-completion-end)))
+      (ivy-read "Command "
+                (delete-dups
+                 (when (> (ring-size eshell-history-ring) 0)
+                   (ring-elements eshell-history-ring)))
+                :initial-input input
+                :action #'ivy-completion-in-region-action))))
+
+(leaf eshell-z
+  :straight t
+  :after eshell
+  :hook (eshell-mode-hook . (lambda () (require 'eshell-z))))
+
+(leaf makefile-executor
+  :straight t)
 
 (provide 'init-sh)
 ;;; init-sh.el ends here
