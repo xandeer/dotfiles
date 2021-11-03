a10c69cd745395646ff187b052f0161483a400bf
Kevin Du <kkxandeer@gmail.com>
2021-11-03 15:50:42 +0800

    feat: set org-roam with embark and ace-windoww

--- emacs.d/.emacs.d/lisp/init-embark.el
+++ emacs.d/.emacs.d/lisp/init-embark.el
@@ -10,14 +10,23 @@
 (leaf embark
   :straight t
   :require t
-  ;; :bind
-  ;; ("C-;" . embark-act)
-  :config
+  :after ivy
+  :bind
+  (:ivy-minibuffer-map
+   ("C-o" . embark-act))
+  :defer-config
   ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
-                 (window-parameters (mode-line-format . none)))))
+                 (window-parameters (mode-line-format . none))))
+  (embark-define-keymap embark-roam-map
+    "Keymap with a roam node actions."
+    ("o" (xr/embark-ace-action org-roam-node-find))
+    ("2" (xr/embark-split-action org-roam-node-find split-window-below))
+    ("3" (xr/embark-split-action org-roam-node-find split-window-right)))
+
+  (add-to-list 'embark-keymap-alist '(org-roam-node . embark-roam-map)))
 
 (provide 'init-embark)
 ;;; init-embark.el ends here
