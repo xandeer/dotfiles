7ce29e412427f95e96832d4ab6ac87071aefe245
Kevin Du <kkxandeer@gmail.com>
2021-11-03 11:12:57 +0800

    feat: use '~/Downloads' as the defalut source directory for org-attach

--- emacs.d/.emacs.d/lisp/init-org.el
+++ emacs.d/.emacs.d/lisp/init-org.el
@@ -158,6 +158,48 @@
                                     "\\|^\\(#\\+end_.*\\)")))
       (setq-local paragraph-start paragraph-ending)
       (setq-local paragraph-separate paragraph-ending)))
+
+  (defun org-attach-attach (file &optional visit-dir method)
+  "Move/copy/link FILE into the attachment directory of the current outline node.
+If VISIT-DIR is non-nil, visit the directory with dired.
+METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
+`org-attach-method'."
+  (interactive
+   (list
+    (read-file-name "File to keep as an attachment: "
+                    (or (progn
+                          (require 'dired-aux)
+                          (dired-dwim-target-directory))
+                        (expand-file-name "~/Downloads")))
+    current-prefix-arg
+    nil))
+  (setq method (or method org-attach-method))
+  (let ((basename (file-name-nondirectory file)))
+    (let* ((attach-dir (org-attach-dir 'get-create))
+           (attach-file (expand-file-name basename attach-dir)))
+      (cond
+       ((eq method 'mv) (rename-file file attach-file))
+       ((eq method 'cp) (copy-file file attach-file))
+       ((eq method 'ln) (add-name-to-file file attach-file))
+       ((eq method 'lns) (make-symbolic-link file attach-file))
+       ((eq method 'url) (url-copy-file file attach-file)))
+      (run-hook-with-args 'org-attach-after-change-hook attach-dir)
+      (org-attach-tag)
+      (cond ((eq org-attach-store-link-p 'attached)
+	     (push (list (concat "attachment:" (file-name-nondirectory attach-file))
+			 (file-name-nondirectory attach-file))
+		   org-stored-links))
+            ((eq org-attach-store-link-p t)
+             (push (list (concat "file:" file)
+			 (file-name-nondirectory file))
+		   org-stored-links))
+	    ((eq org-attach-store-link-p 'file)
+	     (push (list (concat "file:" attach-file)
+			 (file-name-nondirectory attach-file))
+		   org-stored-links)))
+      (if visit-dir
+          (dired attach-dir)
+        (message "File %S is now an attachment" basename)))))
   :advice
   (:after org-setup-filling reset-filling))
 
