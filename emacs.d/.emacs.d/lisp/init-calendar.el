a6a3bdecd1c85a739479987961fcef13043ca458
Kevin Du <kkxandeer@gmail.com>
2021-11-08 16:39:51 +0800

    refactor: give up leaf

--- emacs.d/.emacs.d/lisp/init-calendar.el
+++ emacs.d/.emacs.d/lisp/init-calendar.el
@@ -2,58 +2,53 @@
 ;;; Commentary:
 ;;; Code:
 
-(leaf cal-china-x
-  :straight t
-  :require t)
+(require-package 'cal-china-x)
+(with-eval-after-load 'cal-china-x
+  (require 'holidays)
+  (setq calendar-holidays cal-china-x-chinese-holidays))
 
-(leaf holidays
-  :after cal-china-x
-  :custom
-  (calendar-holidays .  cal-china-x-chinese-holidays))
 ;; (cal-china-x-important-holidays . cal-china-x-chinese-holidays)
 ;; (cal-china-x-general-holidays . '((holiday-lunar 1 15 "元宵节")))
 ;; . (append cal-china-x-important-holidays
 ;; cal-china-x-general-holidays))
 
-(leaf calendar
-  :bind
-  (:calendar-mode-map
-   ("h" . calendar-backward-day)
-   ("l" . calendar-forward-day)
-   ("j" . calendar-forward-week)
-   ("k" . calendar-backward-week)
-   ("H" . calendar-backward-month)
-   ("L" . calendar-forward-month)
-   ("K" . calendar-backward-year)
-   ("J" . calendar-forward-year)
-   ("g t" . calendar-goto-today))
-  (:org-read-date-minibuffer-local-map
-   :package org
-   ("h" . backward-one-day-in-calendar)
-   ("l" . forward-one-day-in-calendar)
-   ("j" . forward-one-week-in-calendar)
-   ("k" . backward-one-week-in-calendar)
-   ("g t" . goto-today-in-calendar))
-  :config
-  (defun goto-today-in-calendar ()
-    (interactive)
-    (org-eval-in-calendar '(calendar-goto-today)))
+(defun goto-today-in-calendar ()
+  (interactive)
+  (org-eval-in-calendar '(calendar-goto-today)))
 
-  (defun forward-one-day-in-calendar ()
-    (interactive)
-    (org-eval-in-calendar '(calendar-forward-day 1)))
+(defun forward-one-day-in-calendar ()
+  (interactive)
+  (org-eval-in-calendar '(calendar-forward-day 1)))
 
-  (defun backward-one-day-in-calendar ()
-    (interactive)
-    (org-eval-in-calendar '(calendar-backward-day 1)))
+(defun backward-one-day-in-calendar ()
+  (interactive)
+  (org-eval-in-calendar '(calendar-backward-day 1)))
 
-  (defun forward-one-week-in-calendar ()
-    (interactive)
-    (org-eval-in-calendar '(calendar-forward-week 1)))
+(defun forward-one-week-in-calendar ()
+  (interactive)
+  (org-eval-in-calendar '(calendar-forward-week 1)))
 
-  (defun backward-one-week-in-calendar ()
-    (interactive)
-    (org-eval-in-calendar '(calendar-backward-week 1))))
+(defun backward-one-week-in-calendar ()
+  (interactive)
+  (org-eval-in-calendar '(calendar-backward-week 1)))
+
+(with-eval-after-load 'calendar
+  (define-key calendar-mode-map (kbd "h") 'calendar-backward-day)
+  (define-key calendar-mode-map (kbd "l") 'calendar-forward-day)
+  (define-key calendar-mode-map (kbd "j") 'calendar-forward-week)
+  (define-key calendar-mode-map (kbd "k") 'calendar-backward-week)
+  (define-key calendar-mode-map (kbd "H") 'calendar-backward-month)
+  (define-key calendar-mode-map (kbd "L") 'calendar-forward-month)
+  (define-key calendar-mode-map (kbd "K") 'calendar-backward-year)
+  (define-key calendar-mode-map (kbd "J") 'calendar-forward-year)
+  (define-key calendar-mode-map (kbd "g t") 'calendar-goto-today))
+
+(with-eval-after-load 'org
+  (define-key org-read-date-minibuffer-local-map (kbd "h") 'backward-one-day-in-calendar)
+  (define-key org-read-date-minibuffer-local-map (kbd "l") 'forward-one-day-in-calendar)
+  (define-key org-read-date-minibuffer-local-map (kbd "j") 'forward-one-week-in-calendar)
+  (define-key org-read-date-minibuffer-local-map (kbd "k") 'backward-one-week-in-calendar)
+  (define-key org-read-date-minibuffer-local-map (kbd "g t") 'goto-today-in-calendar))
 
 (provide 'init-calendar)
 ;;; init-calendar.el ends here
