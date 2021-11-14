55c76f9085244437cb02dd6a8ed67205fd44d913
Kevin Du <kkxandeer@gmail.com>
2021-11-14 17:30:49 +0800

    refactor: remove leaf and ivy

--- emacs.d/.emacs.d/lisp/init-sh.el
+++ emacs.d/.emacs.d/lisp/init-sh.el
@@ -2,41 +2,53 @@
 ;;; Commentary:
 ;;; Code:
 
-(leaf company-shell
-  :straight t
-  :after sh-script
-  :config
-  (add-to-list 'company-backends 'company-shell))
+(require-package 'company-shell)
+(with-eval-after-load 'company
+  (with-eval-after-load 'sh-script
+    (add-to-list 'company-backends 'company-shell)))
 
-(leaf eshell-mode
-  :after org
-  :hook (eshell-mode-hook . (lambda () (define-key eshell-mode-map [(control ?s)] #'xr/eshell-search-history) (company-mode -1)))
-  :custom
-  `(eshell-aliases-file . ,(xr/expand-note "etc/eshell.alias"))
-  :init
-  (defun xr/eshell-search-history ()
-    "Search the eshell command history with ivy."
-    (interactive)
-    (require 'em-hist)
-    (let* ((ivy-completion-beg (eshell-bol))
-           (ivy-completion-end (point-at-eol))
-           (input (buffer-substring-no-properties
-                   ivy-completion-beg
-                   ivy-completion-end)))
-      (ivy-read "Command "
-                (delete-dups
-                 (when (> (ring-size eshell-history-ring) 0)
-                   (ring-elements eshell-history-ring)))
-                :initial-input input
-                :action #'ivy-completion-in-region-action))))
+(with-eval-after-load 'org
+  (setq eshell-aliases-file (xr/expand-note "etc/eshell.alias")))
 
-(leaf eshell-z
-  :straight t
-  :after eshell
-  :hook (eshell-mode-hook . (lambda () (require 'eshell-z))))
+(with-eval-after-load 'eshell
+  (with-eval-after-load 'company
+    (add-hook 'eshell-mode-hook (lambda () (company-mode -1))))
 
-(leaf makefile-executor
-  :straight t)
+  (with-eval-after-load 'consult
+    (defvar consult--eshell-history nil)
+    (defun xr/consult-eshell-history ()
+      "Jump to an eshell history."
+      (interactive)
+      (require 'em-hist)
+      (let* ((completion-beg (eshell-bol))
+             (completion-end (point-at-eol))
+             (input (buffer-substring-no-properties
+                     completion-beg
+                     completion-end))
+             (cand (delete-dups
+                    (when (> (ring-size eshell-history-ring) 0)
+                      (ring-elements eshell-history-ring)))))
+        (end-of-line)
+        (let ((his (consult--read
+                    cand
+                    :prompt "Eshell history: "
+                    :initial input
+                    :sort nil
+                    :require-match t
+                    :history '(:input consult--eshell-history)
+                    :add-history (thing-at-point 'symbol)
+                    :state (consult--insertion-preview completion-beg completion-end))))
+          (when (minibufferp)
+            (delete-minibuffer-contents))
+          (eshell-kill-input)
+          (insert (substring-no-properties his)))))
+    (add-hook 'eshell-mode-hook (lambda () (define-key eshell-mode-map [(control ?s)] #'xr/consult-eshell-history)))))
+
+(require-package 'eshell-z)
+(with-eval-after-load 'eshell
+  (add-hook 'eshell-mode-hook (lambda () (require 'eshell-z))))
+
+(require-package 'makefile-executor)
 
 (provide 'init-sh)
 ;;; init-sh.el ends here
