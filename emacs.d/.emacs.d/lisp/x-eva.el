d4b9f4e0abe20236e5cfbdb5c4b861b44abcedd2
Kevin Du <kkxandeer@gmail.com>
2021-12-09 21:36:48 +0800

    manual: [2021-12-09 Thu 21:36:48]

--- emacs.d/.emacs.d/lisp/x-eva.el
+++ emacs.d/.emacs.d/lisp/x-eva.el
@@ -8,19 +8,22 @@
 
 ;;; Code:
 
-(straight-use-package
+;;; package
+(require-package
  '(eva
    :host github
    :repo "meedstrom/eva"
    :branch "master"
    :files (:defaults "assets" "renv" "*.R" "*.gnuplot")))
 
+;;; init
 (setq eva-cache-dir-path (no-littering-expand-var-file-name "eva"))
 (setq eva-idle-log-path (expand-file-name "idle.tsv" eva-cache-dir-path))
 (setq eva-buffer-info-path (expand-file-name "buffer-info.tsv" eva-cache-dir-path))
 (setq eva-buffer-focus-log-path (expand-file-name "buffer-focus.tsv" eva-cache-dir-path))
 (setq ess-ask-for-ess-directory nil)
 
+;;; config
 (with-eval-after-load 'eva
   (require 'eva-builtin)
   (setq eva--idle-secs-fn #'org-mac-idle-seconds)
