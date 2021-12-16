4efc31d0c004c400a4c248360b6f8bcfa519b17e
Kevin Du <kkxandeer@gmail.com>
2021-12-16 17:00:17 +0800

    manual: [2021-12-16 Thu 17:00:17]

--- emacs.d/.emacs.d/lisp/x-dictionary.el
+++ emacs.d/.emacs.d/lisp/x-dictionary.el
@@ -18,10 +18,12 @@
 (with-eval-after-load 'sdcv
   (set-face-attribute 'sdcv-tooltip-face nil
                       :foreground "#E0F0E9")
+
+  (defun x--sdcv-translate-br (s)
+    (s-replace "<br>" "\n" s))
+  (advice-add 'sdcv-filter :filter-return #'x--sdcv-translate-br)
   (setq sdcv-say-word-p nil)
-  (setq sdcv-dictionary-data-dir (expand-file-name "~/.stardict"))
-  (global-set-key (kbd "C-c x l") 'sdcv-search-pointer)
-  (global-set-key (kbd "C-c x k") 'sdcv-search-input))
+  (setq sdcv-dictionary-data-dir (expand-file-name "~/.stardict")))
 
 (require-package 'go-translate)
 ;; (require 'go-translate)
@@ -33,9 +35,9 @@
   (setq go-translate-base-url "https://translate.google.cn")
   (setq go-translate-local-language "zh-CN")
   (setq go-translate-target-language "en")
-  (setq go-translate-buffer-follow-p t) ; 翻译完成后，总是将光标切换到翻译结果窗口
-  (setq go-translate-buffer-source-fold-p t) ; 在结果页面，折叠源文本。可以通过回车或鼠标点击展开
-  (setq go-translate-buffer-line-wrap-p nil) ; 在结果页面，不允许过长的行折行显示
+  (setq go-translate-buffer-follow-p t) ; 翻译完成后, 总是将光标切换到翻译结果窗口
+  (setq go-translate-buffer-source-fold-p t) ; 在结果页面, 折叠源文本. 可以通过回车或鼠标点击展开
+  (setq go-translate-buffer-line-wrap-p nil) ; 在结果页面, 不允许过长的行折行显示
   (setq go-translate-extra-directions '(("zh-CN" . "zh-TW"))))
 
 (provide 'x-dictionary)
--- emacs.d/.emacs.d/lisp/x-lispy.el
+++ emacs.d/.emacs.d/lisp/x-lispy.el
@@ -7,6 +7,9 @@
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
 
+(setq lispy-completion-method 'default)
+(setq lispy-visit-method 'projectile)
+
 (with-eval-after-load 'lispy
   (define-key lispy-mode-map (kbd "M-j") #'x/ace-goto-char-timer)
   (define-key lispy-mode-map (kbd "M-k") #'x/switch-to-last-buffer)
@@ -23,7 +26,20 @@
     (insert "]"))
 
   (lispy-define-key lispy-mode-map (kbd "[") #'x--lispy-insert-square-left)
-  (lispy-define-key lispy-mode-map (kbd "]") #'x--lispy-insert-square-right))
+  (lispy-define-key lispy-mode-map (kbd "]") #'x--lispy-insert-square-right)
+
+  (define-key lispy-mode-map (kbd "M-e") #'special-lispy-eval-and-comment)
+  (lispy-define-key lispy-mode-map "E" #'lispy-eval-and-replace)
+
+  (defun x--toggle-narrow ()
+    (interactive)
+    (if (buffer-narrowed-p)
+        (widen)
+      (call-interactively #'lispy-narrow)))
+  (lispy-define-key lispy-mode-map "n" #'x--toggle-narrow)
+
+  (lispy-define-key lispy-mode-map "%" #'lispy-delete)
+  (lispy-define-key lispy-mode-map "y" #'lispy-new-copy))
 
 (provide 'x-lispy)
 ;;; x-lispy.el ends here
