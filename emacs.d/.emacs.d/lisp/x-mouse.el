0952fc8d6803d27145b75b5dbcf6ef6ef6902341
Kevin Du <kkxandeer@gmail.com>
2022-04-16 15:02:10 +0800

    feat: enable mouse

--- emacs.d/.emacs.d/lisp/x-mouse.el
+++ emacs.d/.emacs.d/lisp/x-mouse.el
@@ -16,13 +16,12 @@
       scroll-preserve-screen-position 'always
       scroll-error-top-bottom         t)
 
-(require-package 'disable-mouse)
-(require 'disable-mouse)
-(setq disable-mouse-wheel-events nil)
-(global-disable-mouse-mode)
+;; (setq mouse-wheel-scroll-amount '(1))
 
-(setq mouse-wheel-scroll-amount '(1))
-(setq mac-mouse-wheel-smooth-scroll t)
+(require-package 'disable-mouse nil)
+(with-eval-after-load 'disable-mouse
+  (setq disable-mouse-wheel-events nil)
+  (global-disable-mouse-mode))
 
 (provide 'x-mouse)
 ;;; x-mouse.el ends here
