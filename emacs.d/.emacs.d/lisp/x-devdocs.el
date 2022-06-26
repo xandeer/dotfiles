3fa523c2340d50137d8403f6a786aaae20dd288d
Kevin Du <kkxandeer@gmail.com>
2022-06-26 11:36:41 +0800

    manual: [2022-06-26 Sun 11:36:41]

--- emacs.d/.emacs.d/lisp/x-devdocs.el
+++ emacs.d/.emacs.d/lisp/x-devdocs.el
@@ -9,7 +9,8 @@
 
 (add-hook 'css-mode-hook (x/setup-devdocs '("css")))
 (add-hook 'elixir-mode-hook (x/setup-devdocs '("elixir~1.13")))
-(add-hook 'kotlin-mode-hook (x/setup-devdocs '("kotlin~1.6" "openjdk~1.8")))
+(add-hook 'kotlin-mode-hook (x/setup-devdocs '("kotlin~1.6" "openjdk~18")))
+(remove-hook 'kotlin-mode-hook (x/setup-devdocs '("kotlin~1.6" "openjdk~1.8")))
 (add-hook 'typescript-mode-hook (x/setup-devdocs '("typescript")))
 
 (provide 'x-devdocs)
