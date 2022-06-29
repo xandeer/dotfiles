3df438fdaded3f89a214f341a7ddef19f3ed016c
Kevin Du <kkxandeer@gmail.com>
2022-06-29 17:11:09 +0800

    feat: x/devdocs-lookup at point

--- emacs.d/.emacs.d/lisp/x-devdocs.el
+++ emacs.d/.emacs.d/lisp/x-devdocs.el
@@ -2,15 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'thingatpt)
 (require-package 'devdocs)
 
-(defun x/setup-devdocs (docs)
+(defun x/devdocs--setup (docs)
   (lambda () (setq-local devdocs-current-docs docs)))
 
-(add-hook 'css-mode-hook (x/setup-devdocs '("css")))
-(add-hook 'elixir-mode-hook (x/setup-devdocs '("elixir~1.13")))
-(add-hook 'kotlin-mode-hook (x/setup-devdocs '("kotlin~1.6" "openjdk~18")))
-(add-hook 'typescript-mode-hook (x/setup-devdocs '("typescript")))
+(add-hook 'css-mode-hook (x/devdocs--setup '("css")))
+(add-hook 'elixir-mode-hook (x/devdocs--setup '("elixir~1.13")))
+(add-hook 'kotlin-mode-hook (x/devdocs--setup '("kotlin~1.6" "openjdk~18")))
+(add-hook 'typescript-mode-hook (x/devdocs--setup '("typescript")))
+
+(defun x/devdocs-lookup ()
+  "Lookup the symbol at point in devdocs."
+  (interactive)
+  (let ((word (thing-at-point 'symbol)))
+    (if word
+        (devdocs-lookup nil word)
+      (devdocs-lookup))))
 
 (provide 'x-devdocs)
 ;;; x-devdocs.el ends here
