df2061ea9ce8a27d64fdb094c2675bb932491e7c
Kevin Du <kkxandeer@gmail.com>
2022-09-21 21:07:16 +0800

    manual: [2022-09-21 Wed 21:07:16]

--- emacs.d/.emacs.d/early-init.el
+++ emacs.d/.emacs.d/early-init.el
@@ -24,8 +24,11 @@
 (add-to-list 'default-frame-alist '(width . 0.33))
 (add-to-list 'default-frame-alist '(height . 0.66))
 ;; (add-to-list 'default-frame-alist '(alpha . (85 . 78)))
+;; (set-frame-parameter nil 'alpha 90)
 (add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
 (add-to-list 'default-frame-alist '(undecorated . t))
+;; doesn't work on mac
+;; (set-frame-parameter nil 'alpha-background 50)
 
 (setq package-enable-at-startup nil)
 
