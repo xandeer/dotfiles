7b67c13d584b1f6adfa876199824f6c170f3aa1c
Kevin Du <kkxandeer@gmail.com>
2023-03-20 21:16:56 +0800

    manual: [2023-03-20 Mon 21:16:56]

--- emacs.d/.emacs.d/lisp/x-org-attach.el
+++ emacs.d/.emacs.d/lisp/x-org-attach.el
@@ -12,16 +12,17 @@
 
 (with-eval-after-load 'org-attach
   (defun x/org-set-attach-dir ()
-    (interactive)
+    "Set the attachment directory for the current Org buffer."
     (save-excursion
-      (goto-line 1)
-      (org-entry-put (point) "DIR"
-                     (concat (unless (equal default-directory (expand-file-name org-directory))
-                               "../")
-                             ".attach/"
-                             (org-attach-id-uuid-folder-format (org-id-get))))))
+      (goto-char (point-min))
+      (let* ((org-id (org-id-get-create))
+             (attach-dir (concat (unless (equal default-directory (expand-file-name org-directory))
+                                   "../")
+                                 ".attach/"
+                                 (org-attach-id-uuid-folder-format org-id))))
+        (org-entry-put (point) "DIR" attach-dir))))
 
-  (defun org-attach-attach (file &optional visit-dir method)
+  (defun x/org-attach-attach (file &optional visit-dir method)
     "Move/copy/link FILE into the attachment directory of the current outline node.
 If VISIT-DIR is non-nil, visit the directory with dired.
 METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
@@ -49,20 +50,60 @@ METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
         (run-hook-with-args 'org-attach-after-change-hook attach-dir)
         (org-attach-tag)
         (cond ((eq org-attach-store-link-p 'attached)
-	             (push (list (concat "attachment:" (file-name-nondirectory attach-file))
-			                     (file-name-nondirectory attach-file))
-		                 org-stored-links))
+               (push (list (concat "attachment:" (file-name-nondirectory attach-file))
+                           (file-name-nondirectory attach-file))
+                     org-stored-links))
               ((eq org-attach-store-link-p t)
                (push (list (concat "file:" file)
-			                     (file-name-nondirectory file))
-		                 org-stored-links))
-	            ((eq org-attach-store-link-p 'file)
-	             (push (list (concat "file:" attach-file)
-			                     (file-name-nondirectory attach-file))
-		                 org-stored-links)))
+                           (file-name-nondirectory file))
+                     org-stored-links))
+              ((eq org-attach-store-link-p 'file)
+               (push (list (concat "file:" attach-file)
+                           (file-name-nondirectory attach-file))
+                     org-stored-links)))
         (if visit-dir
             (dired attach-dir)
-          (message "File %S is now an attachment" basename))))))
+          (message "File %S is now an attachment" basename)))))
+  (advice-add 'org-attach-attach :override #'x/org-attach-attach)
+
+  (defun x/copy-org-attachments-to-downloads ()
+    "Copy org file attachments to the '~/Downloads' directory."
+    (interactive)
+    (let ((attachments-dir (org-attach-dir))
+          (downloads-dir (expand-file-name "~/Downloads")))
+      (if (and attachments-dir (file-directory-p attachments-dir))
+          (progn
+            (dolist (file (directory-files attachments-dir t))
+              (unless (file-directory-p file)
+                (let ((new-file (expand-file-name (file-name-nondirectory file) downloads-dir)))
+                  (copy-file file new-file t))))
+            (message "Attachments copied to '~/Downloads' directory."))
+        (message "No attachments found."))))
+
+  (defun x/delete-attach-and-properties-block ()
+    "Delete 'ATTACH:' from the heading and the :PROPERTIES: block in the current subtree."
+    (interactive)
+    (save-excursion
+      (org-back-to-heading t)
+      (let ((start (point))
+            (end (org-end-of-subtree)))
+        (goto-char start)
+        (when (re-search-forward "\\s-*ATTACH:" (line-end-position) t)
+          (replace-match ""))
+        (goto-char start)
+        (when (re-search-forward "^\\s-*:PROPERTIES:" end t)
+          (beginning-of-line)
+          (let ((properties-start (point)))
+            (when (re-search-forward "^\\s-*:END:" end t)
+              (forward-line)
+              (delete-region properties-start (point))))))))
+
+  (defun x/org-reattach ()
+    "Reattach files for the current Org node."
+    (interactive)
+    (x/copy-org-attachments-to-downloads)
+    (x/delete-attach-and-properties-block)
+    (funcall-interactively #'org-attach)))
 
 (provide 'x-org-attach)
 ;;; x-org-attach.el ends here
