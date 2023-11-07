3919aa7359cdadf6e203bd743535c652affa1cb1
Kevin Du <kkxandeer@gmail.com>
2023-11-07 09:18:36 +0800

    manual: [2023-11-07 Tue 09:18:36]

--- emacs.d/.emacs.d/lisp/x-jieba.el
+++ emacs.d/.emacs.d/lisp/x-jieba.el
@@ -4,7 +4,7 @@
 
 ;; 使用 jieba 中文分词
 ;; prepare: cd ~/.emacs.d/straight/repos/jieba.el; yarn
-(setq jieba-server-start-args '("nix" "run" "--offline" "nixpkgs#nodejs_18" "--" "simple-jieba-server.js"))
+;; (setq jieba-server-start-args '("nix" "run" "--offline" "nixpkgs#nodejs_18" "--" "simple-jieba-server.js"))
 (x/append-init-hook #'jieba-mode)
 
 (provide 'x-jieba)
