bc94cd74b39610a54493293fc48c072edc96239a
Kevin Du <kkxandeer@gmail.com>
2025-05-02 11:59:14 +0800

    Chore: Change default GPT model from Llama to GPT-4.1 for GitHub
    - Update default value of `x/gpt--model-gh` from 'meta/Llama-4-Maverick-17B-128E-Instruct-FP8 to 'openai/gpt-4.1'
    - Ensures usage of OpenAI's GPT-4.1 model as default for GitHub-related GPT prompts

--- emacs.d/.emacs.d/lisp/x-gpt.el
+++ emacs.d/.emacs.d/lisp/x-gpt.el
@@ -22,7 +22,7 @@
                           :key #'x/gpt--gh-token ;API key
                           :header `(("Authorization" . ,(concat "Bearer " x/gpt--gh-token)))
                           :models '(openai/gpt-4.1 openai/o4-mini meta/Llama-4-Maverick-17B-128E-Instruct-FP8 deepseek/DeepSeek-R1)))
-(setq x/gpt--model-gh 'meta/Llama-4-Maverick-17B-128E-Instruct-FP8)
+(setq x/gpt--model-gh 'openai/gpt-4.1)
 
 ;; deepseek
 (setq x/gpt--ds-token (auth-source-pick-first-password :host "deepseek" :user "ds"))
