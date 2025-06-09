9adcdeb861a122d7849b1958937182ec391da583
Kevin Du <kkxandeer@gmail.com>
2025-06-09 11:54:50 +0800

    Feat: Update Xcode keybindings, add code snippets symlink, and improve linking script
    - Added Default.idekeybindings and new keybinding actions for jump to line, library, and format file shortcuts
    - Removed redundant @f binding from xandeer.idekeybindings and re-assigned it to format file actions in more contexts
    - Introduced symlink for CodeSnippets and updated make-ln.sh to link the entire KeyBindings directory instead of individual files

--- xcode/make-ln.sh
+++ xcode/make-ln.sh
@@ -6,7 +6,5 @@ XCODE_DIR="$HOME/Library/Developer/Xcode/UserData"
 KEYBINDINGS_DIR="$XCODE_DIR/KeyBindings"
 SNIPPETS_DIR="$XCODE_DIR/CodeSnippets"
 
-mkdir -p "$KEYBINDINGS_DIR"
-
-ln -sf "$SCRIPT_DIR"/KeyBindings/*.idekeybindings "$KEYBINDINGS_DIR"
+ln -sf "$SCRIPT_DIR"/KeyBindings "$KEYBINDINGS_DIR"
 ln -sf "$SCRIPT_DIR"/CodeSnippets "$SNIPPETS_DIR"
