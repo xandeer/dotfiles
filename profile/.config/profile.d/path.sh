44a9f139a22dc5e9a358b90fa31e0942d4e6d3f6
Kevin Du <kkxandeer@gmail.com>
2026-03-10 11:55:17 +0800

    refactor: separate shared env from zsh login setup

--- profile/.config/profile.d/path.sh
+++ profile/.config/profile.d/path.sh
@@ -1,2 +1,23 @@
 # Shared PATH setup belongs here.
-# Keep logic portable so it can be reused outside zsh.
+
+case ":$PATH:" in
+  *":$HOME/projects/others/doom/bin:"*)
+    ;;
+  *)
+    if [ -d "$HOME/projects/others/doom/bin" ]; then
+      PATH="${PATH:+$PATH:}$HOME/projects/others/doom/bin"
+    fi
+    ;;
+esac
+
+case ":$PATH:" in
+  *":$HOME/bin:"*)
+    ;;
+  *)
+    if [ -d "$HOME/bin" ]; then
+      PATH="$HOME/bin${PATH:+:$PATH}"
+    fi
+    ;;
+esac
+
+export PATH
--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -1,27 +1,23 @@
-#!/usr/bin/env bash
-
-update_path() {
-  if [[ "$OSTYPE" = darwin* ]]; then
-    # add android path
-    export SDK_DIR=~/Library/Android/sdk
-  fi
-
-  if [[ "$OSTYPE" = linux* ]]; then
-    export SDK_DIR=~/Android/Sdk
-  fi
-
-  export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/Contents/Home
-
-  export PATH=$PATH:$SDK_DIR/platform-tools
-  export PATH=$PATH:$SDK_DIR/tools
-
-  # add doom path
-  export PATH=$PATH:$HOME/projects/others/doom/bin
-
-  [ -d "$HOME/bin" ] && export PATH="$HOME/bin:$PATH"
-}
-
-if [[ "$XANDEER_PROFILE" != loaded ]] ; then
-  export XANDEER_PROFILE=loaded
-  update_path
+profile_path="$HOME/.config/profile.d/path.sh"
+if [[ -r "$profile_path" ]]; then
+  source "$profile_path"
 fi
+
+case "$OSTYPE" in
+  darwin*)
+    profile_os="$HOME/.config/profile.d/darwin.sh"
+    ;;
+  linux*)
+    profile_os="$HOME/.config/profile.d/linux.sh"
+    ;;
+  *)
+    profile_os=""
+    ;;
+esac
+
+if [[ -n "$profile_os" && -r "$profile_os" ]]; then
+  source "$profile_os"
+fi
+
+unset profile_os
+unset profile_path
