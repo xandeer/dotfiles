5dc29e3ddb23cbcebfefd8b467944ead1fc38db8
Kevin Du <kkxandeer@gmail.com>
2026-03-10 12:00:40 +0800

    fix: harden interactive zsh configuration

---
+++ zsh/.config/zsh/interactive/prompt.zsh
@@ -0,0 +1,18 @@
+[[ -n ${ZSH_INTERACTIVE_PROMPT_LOADED:-} ]] && return
+typeset -g ZSH_INTERACTIVE_PROMPT_LOADED=1
+
+if zplug check "denysdovhan/spaceship-prompt"; then
+  SPACESHIP_PROMPT_ORDER=(
+    time
+    dir
+    git
+    exec_time
+    line_sep
+    jobs
+    char
+  )
+
+  SPACESHIP_TIME_SHOW=true
+  SPACESHIP_EXIT_CODE_SHOW=true
+  SPACESHIP_EXEC_TIME_SHOW=true
+fi
