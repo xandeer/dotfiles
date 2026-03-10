ed16dd8aa8d47e0f802d3f68ab60a7bef314a13d
Kevin Du <kkxandeer@gmail.com>
2026-03-10 11:56:52 +0800

    refactor: isolate zplug behind plugin layer

--- zsh/.config/zsh/plugins/zplug.zsh
+++ zsh/.config/zsh/plugins/zplug.zsh
@@ -1,85 +1,8 @@
-#!/bin/zsh -f
-#
-
 echo "Loading zsh plugins..."
 
 export ZPLUG_HOME=$HOME/projects/others/zplug
-source $ZPLUG_HOME/init.zsh
-
-# zplug
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-
-# Load theme
-zplug "mafredri/zsh-async", from:github, use:async.zsh
-# zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme, from:github, at:next, as:theme
-zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-
-zplug "hchbaw/zce.zsh", from:github, use:zce.zsh
-
-# Miscellaneous commands
-# zplug "k4rthik/git-cal", as:command
-# zplug "peco/peco", as:command, from:gh-r, use:"*${(L)$(uname -s)}*amd64*"
-
-# Supports oh-my-zsh plugins and the like
-zplug "lib/completion",                   from:oh-my-zsh
-zplug "lib/directories",                  from:oh-my-zsh
-zplug "lib/functions",                    from:oh-my-zsh
-zplug "lib/git",                          from:oh-my-zsh
-zplug "lib/history",                      from:oh-my-zsh
-# zplug "lib/theme-and-appearance",         from:oh-my-zsh
-zplug "plugins/common-aliases",           from:oh-my-zsh
-zplug "plugins/zsh_reload",               from:oh-my-zsh
-
-if [[ $OSTYPE = (darwin)* ]]; then
-    zplug "lib/clipboard",                from:oh-my-zsh
-    zplug "plugins/osx",                  from:oh-my-zsh
-    # zplug "plugins/brew",                 from:oh-my-zsh, if:"(( $+commands[brew] ))"
-fi
-
-zplug "plugins/adb",                      from:oh-my-zsh, if:"(( $+commands[adb] ))"
-zplug "plugins/cargo",                    from:oh-my-zsh, if:"(( $+commands[cargo] ))"
-zplug "plugins/colored-man-pages",        from:oh-my-zsh
-zplug "plugins/colorize",                 from:oh-my-zsh
-zplug "plugins/command-not-found",        from:oh-my-zsh, if:"(( $+commands[cnf-lookup] ))"
-# zplug "plugins/emacs",                    from:oh-my-zsh, if:"(( $+commands[emacsclient] ))"
-zplug "plugins/git",                      from:oh-my-zsh, if:"(( $+commands[git] ))"
-zplug "plugins/git-extras",               from:oh-my-zsh, if:"(( $+commands[git] ))"
-# zplug "plugins/git-flow-avh",             from:oh-my-zsh, if:"(( $+commands[git] ))"
-zplug "plugins/gitignore",                from:oh-my-zsh, if:"(( $+commands[git] ))"
-zplug "plugins/golang",                   from:oh-my-zsh, if:"(( $+commands[go] ))"
-# zplug "plugins/gradle",                   from:oh-my-zsh, if:"(( $+commands[gradle] ))"
-zplug "plugins/nvm",                      from:oh-my-zsh, if:"(( $+commands[nvm] ))"
-zplug "plugins/pass",                     from:oh-my-zsh, if:"(( $+commands[pass] ))"
-zplug "plugins/rust",                     from:oh-my-zsh, if:"(( $+commands[rust] ))"
-zplug "plugins/rvm",                      from:oh-my-zsh, if:"(( $+commands[rvm] ))"
-zplug "plugins/sudo",                     from:oh-my-zsh, if:"(( $+commands[sudo] ))"
-zplug "plugins/scd",                      from:oh-my-zsh
-export FPATH=$FPATH:$ZPLUG_REPOS/robbyrussell/oh-my-zsh/plugins/scd
-zplug "plugins/systemd",                  from:oh-my-zsh, if:"(( $+commands[systemctl] ))"
-zplug "plugins/yarn",                     from:oh-my-zsh
-
-
-# Warn you when you run a command that you've set an alias for without
-# using the alias.
-zplug "djui/alias-tips"
-
-zplug "hlissner/zsh-autopair", defer:2
-zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-autosuggestions"
-# zsh-syntax-highlighting must be loaded after executing compinit command
-# and sourcing other plugins
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zsh-users/zsh-history-substring-search", defer:3
-
-zplug "Aloxaf/fzf-tab", from:github, if:"(( $+commands[fzf] ))"
-
-# Adds aliases to open your current repo & branch on github.
-# zplug "peterhurford/git-it-on.zsh"
-
-# A collection of scripts that might be useful to sysadmins.
-# zplug "skx/sysadmin-util"
-
-zplug "~/.config/zsh", from:local, defer:3
+source "$ZPLUG_HOME/init.zsh"
+source_if_exists "$ZSH_CONFIG_DIR/plugins/bundles.zsh"
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check; then
@@ -204,6 +127,5 @@ if zplug check "hchbaw/zce.zsh"; then
   bindkey "\eg" zce
 fi
 
-# Then, source plugins and add commands to $PATH
 zplug load
 echo "Zsh plugins loaded."
