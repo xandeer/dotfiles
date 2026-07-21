58a68b0c10a025fcae2eb26cb690a881136db017
Kevin Du <kkxandeer@gmail.com>
2026-07-21 15:03:38 +0800

    fix: avoid shell expansion in RemSpark actions

--- tests/config/emacs-remspark-tracking-actions-regression.zsh
+++ tests/config/emacs-remspark-tracking-actions-regression.zsh
@@ -17,9 +17,11 @@ output="$(
               (defmacro consult-customize (&rest _args) nil)
               (provide 'consult)
               (defun x/define-keys (&rest _args) nil)
-              (defvar test/opened-url nil)
-              (defun x/open (url)
-                (setq test/opened-url url))
+              (defvar test/process-argv nil)
+              (defun x/open (&rest _args)
+                (error \"x/open must not handle RemSpark tracking actions\"))
+              (defun x/start-process (cmd &optional _switch _sentinel)
+                (setq test/process-argv (split-string cmd)))
               (defun org-clock-in-last () (interactive))
               (defun org-clock-out () (interactive))
               (defun x/org-done-current () (interactive)))" \
@@ -30,21 +32,24 @@ output="$(
                      (properties (nth 2 suffix))
                      (description (plist-get properties :description))
                      (command (plist-get properties :command)))
-                (setq test/opened-url nil)
+                (setq test/process-argv nil)
                 (call-interactively command)
-                (princ (format \"%s|%s|%s\\n\"
-                               key description (or test/opened-url \"\")))))"
+                (princ (format \"%s|%s|%d|%s|%s\\n\"
+                               key description
+                               (length test/process-argv)
+                               (or (nth 0 test/process-argv) \"\")
+                               (or (nth 1 test/process-argv) \"\")))))"
 )"
 
 expected_lines=(
-  'i|In last|remspark://trackingAction?action=resume'
-  'l|Out|remspark://trackingAction?action=pause'
-  'k|Done current|remspark://trackingAction?action=complete'
+  'i|In last|2|/usr/bin/open|remspark://trackingAction?action=resume'
+  'l|Out|2|/usr/bin/open|remspark://trackingAction?action=pause'
+  'k|Done current|2|/usr/bin/open|remspark://trackingAction?action=complete'
 )
 
 for expected_line in "${expected_lines[@]}"; do
   print -r -- "$output" | rg -Fx -- "$expected_line" >/dev/null || {
-    print -u2 "expected runtime RemSpark tracking action: $expected_line"
+    print -u2 "expected runtime RemSpark tracking action argv: $expected_line"
     print -u2 -- "$output"
     exit 1
   }
